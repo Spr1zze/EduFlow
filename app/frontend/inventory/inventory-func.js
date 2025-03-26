a833c69d90e1ac984b2a386cfe03cb6c2898ca01
Joachim <brunkartofler@hotmail.com>
2025-03-26 10:30:30 +0100

    Kanban change and re-structuring.

---
+++ app/frontend/inventory/inventory-func.js
@@ -0,0 +1,30 @@
+/* Chart testing */
+const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yValues = [55, 49, 44, 24, 15];
+const barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+    title: {
+      display: true,
+      text: "World Wine Production 2018"
+    }
+  }
+}); 
