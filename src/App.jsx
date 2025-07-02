457e4385930abd3e0e28ac7ea044bb706a6be499
Spr1zze <siedlerbjoachim@hotmail.com>
2025-07-03 01:11:45 +0200

    - Created folder structure and all necessary files - Imported icons for navbar and user profile - Connected BasicLayout component with the main app HTML - Built the basic layout structure; ready for final styling

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  BasicLayout from './components/BasicLayout/BasicLayout.jsx';
+
+export default function App() {
+    return (
+        <>
+            <BasicLayout />
+        </>
+    )
+}
---
+++ src/components/BasicLayout/BasicLayout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './BasicLayout.module.css';
+
+// Importing icons for nav bar 
+import { MdOutlineViewKanban, MdOutlineDashboard, MdOutlineInventory2 } from "react-icons/md";
+import { TbReport } from "react-icons/tb";
+import { PiStudent } from "react-icons/pi";
+import { TiMessages } from "react-icons/ti";
+// Importing user icons
+import { RxAvatar } from "react-icons/rx";
+import { CgArrowsScrollV } from "react-icons/cg";
+
+
+
+// BasicLayout component
+export default function BasicLayout() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.listContainer}>
+                <ul className={styles.sidebarList}>
+                    <li><MdOutlineDashboard />Dashboard</li>
+                    <li><MdOutlineViewKanban />Kanban</li>
+                    <li><TbReport />Batch Report</li>
+                    <li><PiStudent />Student</li>
+                    <li><MdOutlineInventory2 />Inventory</li>
+                    <li><TiMessages />Message</li>        
+                </ul>
+
+                <p className={styles.userProfile}><RxAvatar />Jakob Jensen< CgArrowsScrollV/></p>
+            </div>
+            <div className={styles.whiteBox}></div>
+        </div>
+    )
+}
